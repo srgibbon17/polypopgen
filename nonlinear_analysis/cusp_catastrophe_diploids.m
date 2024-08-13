a0ff86f0eb4d4d3913ab8b841dd045364cecc7ce
srgibbon17 <sgibbon@arizona.edu>
2024-08-13 13:33:52 -0700

    Added mutation equilibrium lines to bifurcation diagrams

--- nonlinear_analysis/cusp_catastrophe_diploids.m
+++ nonlinear_analysis/cusp_catastrophe_diploids.m
@@ -6,7 +6,7 @@ syms s q G0 G1 G2 g0 g1 h mu nu
 % assumptions on the parameters of the model; theoretical bounds
 assume(g0>=0 & g0<=1);
 assume(g1>=0 & g1<=1);
-assume(s>=-1 & s<=1);
+%assume(s>=-1 & s<=1);
 assume(h>=0 & h<=1);
 assume(mu>=0 & mu<=1);
 assume(nu>=0 & nu<=1);
@@ -30,18 +30,35 @@ mut_g1 = sel_g0*mu + sel_g1*(1-nu) - g1*w_bar;
 %removing g1 from the equations
 mut_g0 = subs(mut_g0, g1, 1-g0);
 
-singularity_set = diff(mut_g0, g0);
+mut_g0_coeff = coeffs(simplify(expand(mut_g0)), g0);
+
+mut_g0 = mut_g0/mut_g0_coeff(4);
+
+mut_g0_sub = subs(mut_g0, g0, (g0-(mut_g0_coeff(3)/3)));
+
+mut_g0_sub = (27*s*(2*h - 1))*simplify(expand(mut_g0_sub));
+
+mut_g0_sub_coeff = coeffs(mut_g0_sub, g0);
+
+singularity_set = diff(mut_g0_sub, g0);
 
 isolated_g0 = isolate(singularity_set==0, g0);
 
-equilibria_set = subs(mut_g0, lhs(isolated_g0), rhs(isolated_g0));
+equilibria_set = subs(mut_g0_sub, lhs(isolated_g0), rhs(isolated_g0));
+
+% singularity_set = diff(mut_g0, g0);
+% 
+% isolated_g0 = isolate(singularity_set==0, g0);
+% 
+% equilibria_set = subs(mut_g0, lhs(isolated_g0), rhs(isolated_g0));
 
 eval_eq_set = subs(equilibria_set, mu, mu_val);
 eval_eq_set = subs(eval_eq_set, nu, nu_val);
 
-eval_eq_set = subs(eval_eq_set, h, .9);
+eval_eq_set = subs(eval_eq_set, h, 1);
 
-s_bifurcations = solve(eval_eq_set==0, s, 'ReturnConditions',true);
+s_bifurcations = vpasolve(eval_eq_set==0, s, 0.000001)
+s_bifurcations_2 = vpasolve(eval_eq_set==0, s, .99)
 
 
 
