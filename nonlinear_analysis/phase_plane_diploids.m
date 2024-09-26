acd39904220722a8710475e9e6efb835b123c3f7
srgibbon17 <sgibbon@arizona.edu>
2024-09-26 10:13:16 -0700

    updated autos F-bifn diagrams, decomposed vector fields, and many figures

--- nonlinear_analysis/phase_plane_diploids.m
+++ nonlinear_analysis/phase_plane_diploids.m
@@ -86,83 +86,83 @@ s_unstable = [];
 
 [g0_soln] = numeric_solver(mut_g0==0, mu, mu_val, nu, nu_val, s, s_val, h, h_val, g0);
 
-% for i = 1:length(mu_val)
-%     for j = 1:length(nu_val)
-%         for k = 1:length(s_val)
-%             for l = 1:length(h_val)
-%                 g0_soln_real = [];
-%                 s_soln_real = [];
-%                 [g0_soln] = numeric_solver(mut_g0==0, mu, mu_val(i), nu, nu_val(j), s, s_val(k), h, h_val(l), g0);
-%                 for m = 1:length(g0_soln)
-%                     if imag(g0_soln(m)) == 0
-%                         g0_soln_real(end+1) = g0_soln(m);
-%                         s_soln_real(end+1) = s_val(k);
-%                     end
-%                 end
-%                 for m = 1:length(g0_soln_real)
-%                     if g0_soln_real(m) < min(g0_bifn_values)
-%                         g0_stable_1(end+1) = g0_soln_real(m);
-%                         s_stable_1(end+1) = s_soln_real(m);
-%                     elseif g0_soln_real(m) > min(g0_bifn_values) && g0_soln_real(m) < max(g0_bifn_values)
-%                         g0_unstable(end+1) = g0_soln_real(m);
-%                         s_unstable(end+1) = s_soln_real(m);
-%                     elseif g0_soln_real(m) > max(g0_bifn_values)
-%                         g0_stable_2(end+1) = g0_soln_real(m);
-%                         s_stable_2(end+1) = s_soln_real(m);
-%                     end
-%                 end
-%             end
-%         end
-%     end
-% end
-% 
-% h_str = strcat('h: ',string(h_val));
-% mu_str = strcat('mu: ',string(mu_val));
-% nu_str = strcat('nu: ',string(nu_val));
-% mut_ratio_str = strcat(['mut-ratio ', newline, ...
-%     '(mu/nu): '],string(mut_ratio_val));
-% 
-% parameters_str = {'Parameters:', mu_str, nu_str, mut_ratio_str, h_str};
-% dim = [0.5 0.5 0.3 0.3];
-% 
-% figure 
-% 
-% subplot(1, 2, 1)
-% 
-% plot(s_stable_1, g0_stable_1, 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Stable')
-% hold on
-% plot(s_unstable, g0_unstable, 'LineStyle','--', 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Unstable')
-% plot(s_stable_2, g0_stable_2, 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Stable')
-% plot(s_bifn_values(1), g0_bifn_values(1), '.', 'Color', '#0072BD', 'MarkerSize', 20, 'DisplayName', 'Bifurcation Point 1')
-% plot(s_bifn_values(2), g0_bifn_values(2), '.', 'Color', '#0072BD','MarkerSize', 20, 'DisplayName', 'Bifurcation Point 2')
-% 
-% xlabel('s', 'FontSize', 14)
-% ylabel('g0', 'FontSize', 14)
-% sgtitle('Diploid Bifurcation Diagram', 'FontSize', 16)  
-% xscale log
-% 
-% %xlim([0 1e-6])
-% %ylim([0 1])
-% annotation('textbox', dim, 'String', parameters_str,'FontSize', 10, 'FitBoxToText','on')
-% 
-% subplot(1, 2, 2)
-% 
-% g1_stable_1 = ones(1, length(g0_stable_1)) - g0_stable_1;
-% g1_stable_2 = ones(1, length(g0_stable_2)) - g0_stable_2;
-% g1_unstable = ones(1, length(g0_unstable)) - g0_unstable;
-% 
-% plot(s_stable_1, g1_stable_1, 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Stable')
-% hold on
-% plot(s_unstable, g1_unstable, 'LineStyle','--', 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Unstable')
-% plot(s_stable_2, g1_stable_2, 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Stable')
-% plot(s_bifn_values(1), 1-g0_bifn_values(1), '.', 'Color', '#0072BD', 'MarkerSize', 20, 'DisplayName', 'Bifurcation Point 1')
-% plot(s_bifn_values(2), 1-g0_bifn_values(2), '.', 'Color', '#0072BD','MarkerSize', 20, 'DisplayName', 'Bifurcation Point 2')
-% 
-% xlabel('s', 'FontSize', 14)
-% xscale log
-% 
-% %xlim([0 3e-8])
-% %ylim([0 1])
+for i = 1:length(mu_val)
+    for j = 1:length(nu_val)
+        for k = 1:length(s_val)
+            for l = 1:length(h_val)
+                g0_soln_real = [];
+                s_soln_real = [];
+                [g0_soln] = numeric_solver(mut_g0==0, mu, mu_val(i), nu, nu_val(j), s, s_val(k), h, h_val(l), g0);
+                for m = 1:length(g0_soln)
+                    if imag(g0_soln(m)) == 0
+                        g0_soln_real(end+1) = g0_soln(m);
+                        s_soln_real(end+1) = s_val(k);
+                    end
+                end
+                for m = 1:length(g0_soln_real)
+                    if g0_soln_real(m) < min(g0_bifn_values)
+                        g0_stable_1(end+1) = g0_soln_real(m);
+                        s_stable_1(end+1) = s_soln_real(m);
+                    elseif g0_soln_real(m) > min(g0_bifn_values) && g0_soln_real(m) < max(g0_bifn_values)
+                        g0_unstable(end+1) = g0_soln_real(m);
+                        s_unstable(end+1) = s_soln_real(m);
+                    elseif g0_soln_real(m) > max(g0_bifn_values)
+                        g0_stable_2(end+1) = g0_soln_real(m);
+                        s_stable_2(end+1) = s_soln_real(m);
+                    end
+                end
+            end
+        end
+    end
+end
+
+h_str = strcat('h: ',string(h_val));
+mu_str = strcat('mu: ',string(mu_val));
+nu_str = strcat('nu: ',string(nu_val));
+mut_ratio_str = strcat(['mut-ratio ', newline, ...
+    '(mu/nu): '],string(mut_ratio_val));
+
+parameters_str = {'Parameters:', mu_str, nu_str, mut_ratio_str, h_str};
+dim = [0.5 0.5 0.3 0.3];
+
+figure 
+
+subplot(1, 2, 1)
+
+plot(s_stable_1, g0_stable_1, 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Stable')
+hold on
+plot(s_unstable, g0_unstable, 'LineStyle','--', 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Unstable')
+plot(s_stable_2, g0_stable_2, 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Stable')
+plot(s_bifn_values(1), g0_bifn_values(1), '.', 'Color', '#0072BD', 'MarkerSize', 20, 'DisplayName', 'Bifurcation Point 1')
+plot(s_bifn_values(2), g0_bifn_values(2), '.', 'Color', '#0072BD','MarkerSize', 20, 'DisplayName', 'Bifurcation Point 2')
+
+xlabel('s', 'FontSize', 14)
+ylabel('g0', 'FontSize', 14)
+sgtitle('Diploid Bifurcation Diagram', 'FontSize', 16)  
+xscale log
+
+%xlim([0 1e-6])
+%ylim([0 1])
+annotation('textbox', dim, 'String', parameters_str,'FontSize', 10, 'FitBoxToText','on')
+
+subplot(1, 2, 2)
+
+g1_stable_1 = ones(1, length(g0_stable_1)) - g0_stable_1;
+g1_stable_2 = ones(1, length(g0_stable_2)) - g0_stable_2;
+g1_unstable = ones(1, length(g0_unstable)) - g0_unstable;
+
+plot(s_stable_1, g1_stable_1, 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Stable')
+hold on
+plot(s_unstable, g1_unstable, 'LineStyle','--', 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Unstable')
+plot(s_stable_2, g1_stable_2, 'Color', 'k', 'LineWidth', 1.5, 'DisplayName', 'Stable')
+plot(s_bifn_values(1), 1-g0_bifn_values(1), '.', 'Color', '#0072BD', 'MarkerSize', 20, 'DisplayName', 'Bifurcation Point 1')
+plot(s_bifn_values(2), 1-g0_bifn_values(2), '.', 'Color', '#0072BD','MarkerSize', 20, 'DisplayName', 'Bifurcation Point 2')
+
+xlabel('s', 'FontSize', 14)
+xscale log
+
+%xlim([0 3e-8])
+%ylim([0 1])
 
 
 function [diff_eqn_value] = diff_eqn_eval(mut_exp_g0, mu, mu_value, nu, nu_value, s, s_value, h, h_value, g0, g0_sub_value)
